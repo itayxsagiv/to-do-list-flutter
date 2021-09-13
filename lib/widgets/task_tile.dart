a430e777e059a98fb4e33433fef3641df8364beb
itayxsagiv <itayxsagiv@gmail.com>
2021-09-13 15:06:33 +0300

    Step 5 - State managing

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        '1',
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckBoxState: (newValue) {
+          setState(() {
+            isChecked = newValue!;
+          });
+        },
       ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function(bool?)? toggleCheckBoxState;
+
+  TaskCheckBox(
+      {required this.checkBoxState, required this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
